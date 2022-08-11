0f0fdc96d4a37b6418d4a1e3fb46823492024484
Camila Martinez <camilamarb98@gmail.com>
2022-08-10 20:33:13 -0500

    create grid with array of cards

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 cardsArray = [
     {
-        name: fries,
+        name: 'fries',
         img: 'img/frenchFries.png',
     },
     {
-        name: cookies,
+        name: 'cookies',
         img: 'img/cookies.png',
     },
     {
-        name: bacon,
+        name: 'bacon',
         img: 'img/bacon.png',
     },
     {
-        name: friedEgg,
+        name: 'friedEgg',
         img: 'img/friedEgg.png',
     }
 ]
 
+const gridDisplay = document.querySelector('#grid');
+
+function createBoard() {
+    for (let i = 0; i < cardsArray.length; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'img/bacon.png')
+        card.setAttribute('data-id', i)
+        gridDisplay.appendChild(card)
+
+    }
+}
+
+createBoard()
