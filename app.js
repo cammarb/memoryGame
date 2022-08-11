b554be7f5fad799f582d99d19f4f4f12a9cc46bf
Camila Martinez <camilamarb98@gmail.com>
2022-08-10 19:28:40 -0500

    create array of objects

---
+++ app.js
@@ -0,0 +1,19 @@
+cardsArray = [
+    {
+        name: fries,
+        img: 'img/frenchFries.png',
+    },
+    {
+        name: cookies,
+        img: 'img/cookies.png',
+    },
+    {
+        name: bacon,
+        img: 'img/bacon.png',
+    },
+    {
+        name: friedEgg,
+        img: 'img/friedEgg.png',
+    }
+]
+
